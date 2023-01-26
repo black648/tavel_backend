5db314b0d835dfae360fd1ed785ace5d39c64ba4
HKL <bastlwalzer@gmail.com>
2023-01-26 22:30:38 +0900

    * Application Java To Kotlin

---
+++ src/main/kotlin/com/TravelApplication.kt
@@ -0,0 +1,10 @@
+package com
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelApplication
+    fun main(args: Array<String>) {
+        runApplication<TravelApplication>(*args)
+    }
